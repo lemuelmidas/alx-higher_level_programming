b63c3ce57b6e13576d2a1c3899484b622357b6f4
Lemuel Midas <xerxes50@yahoo.com>
2024-02-20 09:17:08 -0800

    Correction for task o

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Lists all states from the database hbtn_0e_0_usa"""
-import MySqldb
+import MySQLdb
 import sys
 
-if __name__ = "__main__":
-    conn = MySqldb.connect(host = "localhost",
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host = "localhost",
                            port= 3306,
                            user= sys.argv[1],
                            passwd= sys.argv[2],
